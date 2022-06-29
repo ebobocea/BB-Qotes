754741f097de11e6f59698004443a1f1701de373
Elisei Bobocea <elisey93@icloud.com>
2022-06-29 12:59:58 +0100

    Initial Commit

---
+++ BB Qotes/BB_QotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BB_QotesApp.swift
+//  BB Qotes
+//
+//  Created by Elisei Bobocea on 29/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct BB_QotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BB Qotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BB Qotes
+//
+//  Created by Elisei Bobocea on 29/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
